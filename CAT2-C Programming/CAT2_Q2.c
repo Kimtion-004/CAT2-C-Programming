76136a34176ed3b03ee136c8392696aab286ae6d
Kimtion-004 <gkimemia21@gmail.com>
2024-11-07 22:35:03 +0400

    Add files via upload

---
+++ CAT2-C Programming/CAT2_Q2.c
@@ -0,0 +1,19 @@
+//George Karanja
+//CT1O2/G/22502/24
+#include <stdio.h>
+
+int main() {
+    int scores[2][4] = {
+        {65, 92, 35, 70},
+        {84, 72, 59, 67}
+    };
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", scores[i][j]);
+        }
+        printf("\n");  // Newline after each row
+    }
+
+    return 0;
+}
